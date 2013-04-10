3c76f0b1c3e845581a76f8cab518d5bef38d2298
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-04-10 17:42:03 -0400

    Twitter::Client now accepts the tweet data as argument in create_tweet method.

--- business_logic/twitter_client.rb
+++ business_logic/twitter_client.rb
@@ -3,13 +3,9 @@ require_relative "./tweet"
 
 module Twitter
   class Client
-    def initialize(tweet_data)
-      @tweet_data = tweet_data
-    end
-
-    def create_tweet
-      Tweet.new(@tweet_data["user"]["screen_name"], 
-                @tweet_data["text"])
+    def create_tweet(tweet)
+      Tweet.new(tweet["user"]["screen_name"], 
+                tweet["text"])
     end
   end
 end
--- business_logic/twitter_client_spec.rb
+++ business_logic/twitter_client_spec.rb
@@ -1,26 +1,19 @@
 require_relative "./twitter_client"
+require_relative "./data"
 
 module Twitter
   describe Client do
     context "#tweet_data_parsing" do
-      before do
-        @client = Twitter::Client.new(individual_tweet_hash)
-      end
-
       it "creates a tweet from raw data" do
-        tweet = @client.create_tweet
+        tweet = subject.create_tweet(one_tweet_data)
         tweet.screen_name.should == "masihjesus"
         tweet.text.should == 
           "Do a little bit of something &amp; get some feedback from it as quick as U can! It is not just for programming! This is also good habit in life!"
       end
     end
 
-    def individual_tweet_hash
-      data_path = File.expand_path(File.dirname(__FILE__) + "/one_tweet.data")
-      tweet = File.open(data_path, "r") do |f|
-        f.read
-      end
-      JSON.parse tweet
+    def one_tweet_data
+      JSON.parse Data.one_tweet
     end
   end
 end
