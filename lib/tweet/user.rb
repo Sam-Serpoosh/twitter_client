d527513b3fb64d9e975cfeea54c99873499d1c55
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-04-27 01:32:18 -0400

    can add to friends of a user

--- lib/tweet/user.rb
+++ lib/tweet/user.rb
@@ -1,10 +1,19 @@
 module Twitter
   class User
-    attr_reader :id, :screen_name
+    attr_reader :id, :screen_name, :friends
 
     def initialize(id, screen_name)
       @id = id
       @screen_name = screen_name
+      @friends = []
+    end
+
+    def add_friends(friends)
+      @friends += friends
+    end
+
+    def to_s
+      "#{id} - #{screen_name}"
     end
   end
 end
--- lib/tweet_spec/user_spec.rb
+++ lib/tweet_spec/user_spec.rb
@@ -1,13 +1,29 @@
 require_relative "../tweet/user"
 
-describe User do
-  let(:user) { User.new(1, "masihjesus") }
+module Twitter
+  describe User do
+    let(:user) { User.new(1, "masihjesus") }
 
-  it "sets the id in creation" do
-    user.id.should == 1
-  end
+    it "sets the id in creation" do
+      user.id.should == 1
+    end
 
-  it "sets the user screen name at creation" do
-    user.screen_name.should == "masihjesus"
+    it "sets the user screen name at creation" do
+      user.screen_name.should == "masihjesus"
+    end
+
+    it "has friends and it's empty at the beginning" do
+      user.friends.should be_empty
+    end
+
+    it "add to the existing friends" do
+      user = User.new(1, "masihjesus")
+      bob = User.new(2, "bob")
+      alice = User.new(3, "alice")
+      user.add_friends([bob])
+      user.add_friends([alice])
+
+      user.friends.should == [bob, alice]
+    end
   end
 end
