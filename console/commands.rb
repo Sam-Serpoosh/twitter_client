19a2c009da3828ba7fbca7652efcf910f2a32cd5
Sam-Serpoosh <sam.serpoosh@gmail.com>
2013-05-22 23:14:32 -0500

    Introduce Commands classes and get ride of if/else chain.

---
+++ console/commands.rb
@@ -0,0 +1,81 @@
+module Twitter
+  module Commands
+    TIMELINE = "timeline"
+    MENTIONS = "mention"
+    RETWEETS = "rets"
+    FRIENDS = "friends"
+    EXIT = "exit"
+  end
+
+  class TimelineCommand
+    def match?(command)
+      command == Commands::TIMELINE
+    end
+
+    def execute
+      TwitterLib.user_timeline("masihjesus")
+    end
+  end
+
+  class MentionsCommand
+    def match?(command)
+      command == Commands::MENTIONS
+    end
+
+    def execute
+      TwitterLib.mentions_timeline
+    end
+  end
+
+  class RetweetsCommand
+    def match?(command)
+      command == Commands::RETWEETS
+    end
+
+    def execute
+      TwitterLib.retweets
+    end
+  end
+
+  class FriendsUpdateCommand
+    def match?(command)
+      command == Commands::FRIENDS
+    end
+
+    def execute
+      TwitterLib.friends_latest_tweets
+    end
+  end
+
+  class ExitCommand
+    def match?(command)
+      command == Commands::EXIT
+    end
+
+    def execute
+      nil
+    end
+  end
+
+  class NotExistCommand
+    def match?(command)
+      true
+    end
+
+    def execute
+      puts valid_commands_message
+      []
+    end
+
+    private
+
+    def valid_commands_message
+      %Q{
+          Valid commands are:
+            "timeline" --> tweet timeline
+            "mentions" --> your mentions
+            "rets"     --> your retweets
+      }
+    end
+  end
+end
